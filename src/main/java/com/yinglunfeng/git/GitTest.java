31f6591cd6a2d12f19ada2fc00b3449d5176ee19
yinglunfen <125241258+yinglunfen@users.noreply.github.com>
2023-05-18 16:47:19 +0800

    pull commit

--- src/main/java/com/yinglunfeng/git/GitTest.java
+++ src/main/java/com/yinglunfeng/git/GitTest.java
@@ -11,6 +11,7 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
     }
 
 }
